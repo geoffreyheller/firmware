28c659e0b0228a6e1f7d7208f221d0bcb9c9e272
geoffreyheller <geoffrey@frostpaw.com>
2025-08-17 16:49:30 -0700

    Conform to new file structure

--- variants/esp32s3/diy/s3-devkitc1-n16r8-E22/variant.h
+++ variants/esp32s3/diy/s3-devkitc1-n16r8-E22/variant.h
@@ -1,8 +1,5 @@
 // DIY n16r8 with Ebyte E22-900M30S
 
-
-// TODO: Might want to try using SERIAL1 GPIO 17 & 18
-
 // Buttons
 #define BUTTON_PIN 0 // Use the BOOT button as the user button
 
@@ -24,16 +21,10 @@
 #define SX126X_MAX_POWER 22          // SX126xInterface.cpp defaults to 22 if not defined, but here we define it for good practice
 #define SX126X_DIO3_TCXO_VOLTAGE 1.8 // E22 series TCXO reference voltage is 1.8V
 
-// The SPI2 (VSPI) default pins are:
-// gpio.12 SCK Defined as SPI0_SCK
-// gpio.11 MOSI Defined as SPI0_MOSI
-// gpio.13 MISO Defined as SPI0_MISO
-// gpio.10 CSO Defined as SPI_CS0
-
-#define SX126X_CS 10    // EBYTE module's NSS pin // FIXME: rename to SX126X_SS
-#define SX126X_SCK 12   // EBYTE module's SCK pin
-#define SX126X_MOSI 11  // EBYTE module's MOSI pin
-#define SX126X_MISO 13  // EBYTE module's MISO pin
+#define SX126X_CS 10    // EBYTE module's NSS pin - esp32s3 VSPI defaults SPI_CS0 to gpio.10
+#define SX126X_SCK 12   // EBYTE module's SCK pin - esp32s3 VSPI defaults SPI0_SCK to gpio.12
+#define SX126X_MOSI 11  // EBYTE module's MOSI pin - esp32s3 VSPI defaults SPI0_MOSI to gpio.11
+#define SX126X_MISO 13  // EBYTE module's MISO pin - esp32s3 VSPI defaults SPI0_MISO to gpio.13
 #define SX126X_RESET 40 // EBYTE module's NRST pin
 #define SX126X_BUSY 41  // EBYTE module's BUSY pin
 #define SX126X_DIO1 42  // EBYTE module's DIO1 pin
